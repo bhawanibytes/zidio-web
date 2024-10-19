0ddf970342990d6313e102cc9d92aae5c18af6f4
Bhawani Singh <bs190520@gmail.com>
2024-10-20 03:07:33 +0530

    added cursor pointer in Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
         </div>
         <div className='flex items-center'>
             <ul className='list-none flex'>
-                <li className='mx-5'>Home</li>
-                <li className='mx-5'>About</li>
-                <li className='mx-5'>Services</li>
-                <li className='mx-5'>Contact</li>
+                <li className='mx-5 hover:text-btColor cursor-pointer'>Home</li>
+                <li className='mx-5 hover:text-btColor cursor-pointer'>About</li>
+                <li className='mx-5 hover:text-btColor cursor-pointer'>Services</li>
+                <li className='mx-5 hover:text-btColor cursor-pointer'>Contact</li>
             </ul>
             
-            <span className='mx-5'>🔍</span>
+            <span className='mx-5 cursor-pointer'>🔍</span>
 
             <button className='mx-5 px-5 py-3 bg-btColor text-white rounded-md hover:bg-white hover:text-black outline outline-1 outline-btColor'>Get A Quote</button>
         </div>
